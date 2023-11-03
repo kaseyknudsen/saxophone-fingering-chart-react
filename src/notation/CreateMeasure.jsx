e0803058e31708d6ca53bc7924b7035c4a1b89bd
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-11-03 14:46:17 -0700

    added state

--- src/notation/CreateMeasure.jsx
+++ src/notation/CreateMeasure.jsx
@@ -1,9 +1,10 @@
 import Vex from "vexflow";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import AddNotes from "./AddNotes";
 import AddModifier from "./AddModifier";
 import noteArray from "./noteData";
 const { Renderer, Stave, Accidental } = Vex.Flow;
+
 const CreateMeasure = ({
   renderWidth,
   renderHeight,
@@ -14,26 +15,29 @@ const CreateMeasure = ({
   timeSignature,
 }) => {
   const notationRef = useRef(null);
+  const [vexContext, setVexContext] = useState(null);
+  const [vexStave, setVexStave] = useState(null);
 
   useEffect(() => {
     if (notationRef.current) {
-      console.log(noteArray);
+      
       const renderer = new Renderer(
         notationRef.current.id,
         Renderer.Backends.SVG
       );
-      renderer.resize(renderWidth, renderHeight);
       const context = renderer.getContext();
-      const stave = new Stave(staveX, staveY, staveWidth);
-      stave.addClef(clef).addTimeSignature(timeSignature);
-      stave.setContext(context).draw();
-
-      AddNotes(10, 4, context, stave, noteArray);
+      renderer.resize(renderWidth, renderHeight);
+      setVexContext(renderer.getContext());
+      const stave = new Stave(staveX, staveY, staveWidth)
+        .addClef(clef)
+        .addTimeSignature(timeSignature)
+        .setContext(context)
+        .draw();
+      setVexContext(context);
+      setVexStave(stave);
 
       return () => {
-        if (notationRef.current) {
-          notationRef.current.innerHTML = "";
-        }
+        notationRef.current.innerHTML = "";
       };
     }
   }, [
@@ -48,7 +52,15 @@ const CreateMeasure = ({
 
   return (
     <>
-      <div ref={notationRef} id="notation-root"></div>
+      <div ref={notationRef} id="notation-root">
+        <AddNotes
+          num_beats={noteArray.length}
+          beat_value={4}
+          context={vexContext}
+          stave={vexStave}
+          notesArray={noteArray}
+        />
+      </div>
     </>
   );
 };
