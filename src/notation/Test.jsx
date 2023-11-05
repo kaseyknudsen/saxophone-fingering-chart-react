8cafab27b58157a09fc4bb7019eee4af5117f726
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-11-05 10:09:48 -0800

    moved MP3s into the public folder

--- src/notation/Test.jsx
+++ src/notation/Test.jsx
@@ -1,7 +1,7 @@
 import Vex from "vexflow";
 const { Stave, Accidental, Renderer, StaveNote, Voice, Formatter } = Vex.Flow;
 import { useEffect, useRef, useState } from "react";
-import B from "../audio/chr_scale/Middle-B.mp3";
+import noteArray from "./noteData";
 
 const Test = () => {
   const noteRef = useRef(null);
@@ -12,7 +12,6 @@ const Test = () => {
   useEffect(() => {
     //create staff
     const renderer = new Renderer(noteRef.current, Renderer.Backends.SVG);
-    // console.log(renderer)
     renderer.resize(600, 600);
     const context = renderer.getContext();
     const stave = new Stave(10, 40, 400);
@@ -23,19 +22,16 @@ const Test = () => {
     new Formatter().joinVoices([voice]).format([voice], 150);
     voice.draw(context, stave);
 
-    //add audio
     // Event listener for notes
-    const allSVGgs = document.querySelectorAll("svg g.vf-notehead");
-    console.log(allSVGgs);
-    for (const svg of allSVGgs) {
+    const allSVGgs = noteRef.current.querySelectorAll("svg g.vf-notehead");
+    allSVGgs.forEach((svg, idx) => {
       svg.addEventListener("click", playAudio);
-    }
-
+    });
     /* create AudioContext -> update context state -> fetch audio -> create AudioBuffer for MP3 file -> decode buffer -> update buffer state -> load audio*/
     const newContext = new AudioContext();
     setAudioContext(newContext);
     const loadAudio = async () => {
-      const response = await fetch(B); // The path to your MP3 file
+      const response = await fetch(noteArray[13].audioPath); // The path to your MP3 file
       const arrayBuffer = await response.arrayBuffer();
       newContext.decodeAudioData(arrayBuffer, (buffer) => {
         setAudioBuffer(buffer);
@@ -44,9 +40,16 @@ const Test = () => {
 
     loadAudio();
 
+    // cleanup Event listener for notes
+
     return () => {
+      allSVGgs.forEach((svg) => {
+        svg.removeEventListener("click", playAudio);
+      });
+      if (audioContext) {
+        audioContext.close();
+      }
       noteRef.current.innerHTML = "";
-      //   newContext.close();
     };
   }, []);
 
@@ -60,17 +63,12 @@ const Test = () => {
       source.start(0); // Play the sound now
     }
   };
-  // Event listener for notes
 
   return (
     <>
       <div ref={noteRef} id="notation-root"></div>
-      <div>
-        <button onClick={playAudio}>Play B</button>
-      </div>
     </>
   );
 };
 
 export default Test;
-//sax-fingering-chart/src/audio/alto_sax_chromatic_scale /Low-Bb.mp3
