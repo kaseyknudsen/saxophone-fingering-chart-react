bcd40208fb7c29b3853c9376cd2944c24776a825
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-11-04 18:52:45 -0700

    made button that plays 1 note using web audio

--- src/notation/Test.jsx
+++ src/notation/Test.jsx
@@ -1,11 +1,13 @@
 import Vex from "vexflow";
 const { Stave, Accidental, Renderer, StaveNote, Voice, Formatter } = Vex.Flow;
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import B from "../audio/chr_scale/Middle-B.mp3";
 
 const Test = () => {
   const noteRef = useRef(null);
-  const audioContextRef = useRef(null);
+//   const audioContextRef = useRef(null);
+  const [audioContext, setAudioContext] = useState(null);
+  const [audioBuffer, setAudioBuffer] = useState(null);
 
   useEffect(() => {
     //create staff
@@ -20,15 +22,41 @@ const Test = () => {
     new Formatter().joinVoices([voice]).format([voice], 150);
     voice.draw(context, stave);
     //add audio
-    audioContextRef.current = new AudioContext();
+
+    const newContext = new AudioContext();
+    setAudioContext(newContext);
+    const loadAudio = async () => {
+      const response = await fetch(B); // The path to your MP3 file
+      const arrayBuffer = await response.arrayBuffer();
+      newContext.decodeAudioData(arrayBuffer, (buffer) => {
+        setAudioBuffer(buffer);
+      });
+    };
+
+    loadAudio();
+
     return () => {
       noteRef.current.innerHTML = "";
+      newContext.close();
     };
   }, []);
+
+  const playAudio = () => {
+    if (audioContext && audioBuffer) {
+      // Create a buffer source
+      const source = audioContext.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(audioContext.destination); // Connect to the speakers
+      source.start(); // Play the sound now
+    }
+  };
 
   return (
     <>
       <div ref={noteRef} id="notation-root"></div>
+      <div>
+        <button onClick={playAudio}>Play B</button>
+      </div>
     </>
   );
 };
